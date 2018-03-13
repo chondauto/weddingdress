096cf0983aa3d5e04b41269754ea1ded05466b33
YOUR NAME <YOUR@EMAIL.com>
2018-03-13 20:07:57 +0700

    remove redandunt file

--- app/models/weddingdress.rb
+++
@@ -1,2 +0,0 @@
-class Weddingdress < ApplicationRecord
-end
--- test/models/weddingdress_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class WeddingdressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
